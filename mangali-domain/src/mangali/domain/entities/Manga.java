f9a1fa7b38c4070f61c83b059b9ca5b07be7b736
Marco Antonio Parente Nunes <marcoparente@Marco.local>
2019-07-31 20:03:13 -0400

    Adicionadas as entidades Manga, Volume e Capitulo.

---
+++ mangali-domain/src/mangali/domain/entities/Manga.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mangali.domain.entities;
+
+/**
+ *
+ * @author marcoparente
+ */
+public class Manga {
+    private int id;
+    private String nome;
+    private String sinopse;
+    private boolean ativo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+    
+    
+}
