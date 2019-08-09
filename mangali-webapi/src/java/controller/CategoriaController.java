6839e3cc55f9dbfc335ee70f11786c44fd09b0fb
Marco Antonio Parente Nunes <marcoparente@Marco.local>
2019-08-09 17:12:37 -0400

    Adicinando a implementacao de cadastrar e selecionar categoria

--- mangali-webapi/src/java/controller/CategoriaController.java
+++ mangali-webapi/src/java/controller/CategoriaController.java
@@ -2,13 +2,53 @@ package controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import mangali.domain.dao.ICategoriaDAO;
+import mangali.domain.dao.list.CategoriaDAOImpl;
+import mangali.domain.entities.Categoria;
 
 @Path("/categoria")
 public class CategoriaController {
     
+    private ICategoriaDAO banco = new CategoriaDAOImpl();
+    
     @GET
     @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
     public String index() {
-        return "teste";
+        return "\"teste\"";
+    }
+    
+    @GET
+    @Path("/select/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Categoria select(@PathParam("id") int pk){
+        System.out.println("Parametro: "+pk);
+        for(Categoria cat : banco.consultar()){
+            if(cat.getId() == pk){
+                return cat;
+            }
+        }
+        return null;
+    }
+    
+    @GET
+    @Path("/insert/{nome}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String cadastrar(@PathParam("nome") String nome){
+        
+        try{
+            System.out.println("Cadastrando " + nome);
+            Categoria nova = new Categoria();
+        nova.setNome(nome);
+        banco.inserir(nova);
+        } catch (Exception erro) {
+            return "{\"status\": 0}";
+        }
+        
+        String ret = "{\"status\" : 1}";
+        return ret;
     }
 }
